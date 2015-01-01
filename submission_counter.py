54cca3cb795666234ee844d36360d93236135c88
shubhdev <shubhamrawat1100@gmail.com>
2015-01-01 15:38:00 +0530

    Added the first script

---
+++ submission_counter.py
@@ -0,0 +1,93 @@
+import sys
+import urllib
+from bs4 import BeautifulSoup
+print ("Enter the problem link (add http:// if not present):")
+x = raw_input()
+pos = x.find('problems')
+url = ''
+#print pos
+if pos == -1:
+	print "The problem link is different than what was expected ( expected format: .../problems/...).\nGo to the 'All submissions' link and enter the link of the opened page : "
+	url = raw_input()
+else :
+	url=x[:pos]+'status'+x[(pos+8):]
+
+const_url_suffix = '?sort_by=All&sorting_order=asc&language=All&status=15&handle=&Submit=GO'   #gets the page in which there are only AC solutions
+url += const_url_suffix
+page = urllib.urlopen(url)
+page = page.read()
+soup = BeautifulSoup(page)
+#soup.prettify()
+#print soup
+#pageinfo = soup.find('div',class_='pageinfo').string
+#totalpages = int(pageinfo[5:])
+#print totalpages
+count = 0
+pageno = 0
+found = []
+#page = urllib.urlopen(url)
+#page = page.read()
+#soup = BeautifulSoup(page)
+#soup.prettify()
+proceed = True
+while proceed:
+	#print url
+	page = urllib.urlopen(url)
+	page = page.read()
+	soup = BeautifulSoup(page)
+	pageno +=1
+	#print pageno
+
+	submission_rows = soup.find_all('tr',class_='kol')
+	#print submission_rows.get_text()
+	for row in submission_rows:
+		cnt = 1
+		username = ''
+		score = 0
+		for string in row.strings: 							#the text in one row
+			if cnt == 3 : username = string     			#gives the 3rd value in the row i.e the username
+			if cnt == 4 : 									#gives the 4th value in the row i.e score
+				score = int(string)
+				if ( found.count(username) == 0 and score == 100):
+	 				found.append(username)
+	 				count += 1
+	 			break
+	 		cnt += 1
+	# 	username = ''
+	# 	score = 0
+	# 	for fields in row.find_all('td'):
+	# 	
+	# 		if fields['width'] == '144' :
+	# 			username = fields.find('a')['title']
+	# 			#print username
+	# 		if fields['width'] == '51':
+	# 			score = int(fields.find(text = True))
+	# 		if ( found.count(username) == 0 and score == 100):
+	# 			found.append(username)
+	# 			count += 1
+	proceed = False
+	for nextbutton in soup.find_all('a',class_='active') :
+		if nextbutton.find('img')['src'].find('next') != -1 :
+			url = 'http://www.codechef.com' + nextbutton['href']
+			proceed = True
+			#print nextbutton['href']
+		
+print "Number of distinct users with 100 points : ",count
+# y = "http://www.codechef.com/"
+# y += x
+# print (y)
+# page = urllib2.urlopen(y)
+# #print (page.geturl().strip(y))
+# if page.geturl() != y:
+#     print ("Problem does not exist")
+#     sys.exit(0)
+# page = page.read()
+# soup = BeautifulSoup(page)
+# soup.prettify()
+# #print (soup)
+# mainblock = soup.find('div', class_ = "node clear-block")
+# #print (mainblock)
+# #print (mainblock)
+# for paras in mainblock.find('div',class_="content").find_all(['p','h3','pre']):
+#    for yo in paras.contents:
+#        print (yo)
